a782376a25a25e1a6a5178160b74e0ba842cb76e
Opus1no2 <tillotson.travis@gmail.com>
2020-03-28 12:49:45 -0500

    update styles

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_s3_bucket" "snake" {
+resource "aws_s3_bucket" "sudoku" {
   bucket        = "sudoku-game.com"
   acl           = "public-read"
   force_destroy = true
@@ -11,5 +11,21 @@ resource "aws_s3_bucket" "snake" {
   website {
     index_document = "index.html"
     error_document = "index.html"
+  }
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "sudoku-game.com"
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "sudoku-game.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket.sudoku.website_domain
+    zone_id                = aws_s3_bucket.sudoku.hosted_zone_id
+    evaluate_target_health = true
   }
 }
